cb5b18f5ec4c3a0717f7ff4d22860278e2d4331c
Harshitha S M <harshithaharshu98845@gmail.com>
2021-10-07 23:32:45 +0530

    Add devise and create users stocks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_05_181854) do
+ActiveRecord::Schema.define(version: 2021_10_06_140242) do
+
+  create_table "stocks", force: :cascade do |t|
+    t.string "ticker"
+    t.string "name"
+    t.decimal "last_price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
